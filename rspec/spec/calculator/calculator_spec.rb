d2f8c8692d7c9a3ac6cb2f891813148eab22ce96
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2019-09-05 17:38:15 -0300

    use subject rspec variable

--- rspec/spec/calculator/calculator_spec.rb
+++ rspec/spec/calculator/calculator_spec.rb
@@ -1,15 +1,23 @@
 require 'calculator'
 
 describe Calculator do
-  it 'use sum method for 2 numbers' do
-    result = Calculator.new.sum(1, 2)
-
-    expect(result).to eq(3)
-  end
+  context '#sum' do
+    it 'with positives numbers' do
+      result = subject.sum(1, 2)
   
-  it 'use sum method for 2 negatives numbers' do
-    result = Calculator.new.sum(-1, -2)
-
-    expect(result).to eq(-3)
+      expect(result).to eq(3)
+    end
+  
+    it 'with negatives and positives numbers' do
+      result = subject.sum(1, -2)
+  
+      expect(result).to eq(-1)
+    end
+  
+    it 'with negatives numbers' do
+      result = subject.sum(-1, -2)
+  
+      expect(result).to eq(-3)
+    end
   end
 end
